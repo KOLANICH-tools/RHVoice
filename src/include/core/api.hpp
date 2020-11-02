aeafd73d0620c8bba0fc3fb7c692e89d03e3c2b0
KOLANICH <kolan_n@mail.ru>
2020-11-02 14:06:45 +0300

    Resolved cyclic dependencies between some parts of the lib to allow dynamic linking to be used on Windows platform. Also resolved some issues related to CPack packaging.

--- src/include/core/api.hpp
+++ src/include/core/api.hpp
@@ -3,11 +3,11 @@
     #define RHVOICE_EXPORT_API __declspec(dllexport)
     #define RHVOICE_IMPORT_API __declspec(dllimport)
 #else
-  #if defined(_WIN32)
-    #define RHVOICE_EXPORT_API [[gnu::dllexport]]
-    #define RHVOICE_IMPORT_API [[gnu::dllimport]]
+  #if defined(_WIN32) && _WIN32
+    #define RHVOICE_EXPORT_API __attribute__((dllexport))
+    #define RHVOICE_IMPORT_API __attribute__((dllimport))
   #else
-    #define RHVOICE_EXPORT_API [[gnu::visibility("default")]]
+    #define RHVOICE_EXPORT_API __attribute__((visibility("default")))
     #define RHVOICE_IMPORT_API
   #endif
 #endif
--- src/include/core/bpf.h
+++ src/include/core/bpf.h
@@ -15,6 +15,15 @@
 
 #ifndef RHVOICE_BPF_H
 #define RHVOICE_BPF_H
+
+#include "api.hpp"
+
+#ifdef RHVOICE_FILTER_MANAGER_EXPORTS
+#    define RHVOICE_FILTER_MANAGER_API RHVOICE_EXPORT_API
+#else
+#    define RHVOICE_FILTER_MANAGER_API RHVOICE_IMPORT_API
+#endif
+
 #ifdef __cplusplus
 extern "C" {
   #endif
@@ -26,9 +35,9 @@ typedef struct
   double** coefs;
 } BPF;
 
-  void bpf_init(BPF* bpf);
-  void bpf_clear(BPF* bpf);
-  int bpf_load(BPF* bpf,const char* path);
+  RHVOICE_FILTER_MANAGER_API void bpf_init(BPF* bpf);
+  RHVOICE_FILTER_MANAGER_API void bpf_clear(BPF* bpf);
+  RHVOICE_FILTER_MANAGER_API int bpf_load(BPF* bpf,const char* path);
 #ifdef __cplusplus
 }
 #endif
